1c6b6feda796ac763d55f0b580efa2467870154b
ElenaKuklina <cuklina.lena@gmail.com>
2024-03-16 13:49:51 +0300

    Tuesday_DOP_4_week: new syntax: router, Outlet, createBrowserRouter([]), RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
 import React, {useState} from 'react';
+import {NavLink, Outlet} from 'react-router-dom';
 import './App.css';
 import {Site} from "./components/Site";
+import styles from "./components/Site.module.css";
+import {S} from "./components/pages/__styles";
 
 
 function App() {
     return (
         <div>
-            <Site/>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            {/* подтянули шапку для сайта*/}
+            <div className={styles.body}> {/*стили для body*/}
+                <div className={styles.nav}> {/*подтянули наши кнопки правый сайдбар*/}
+                    <S.NavWrapper><NavLink to={'/page/0'}>Page1</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={'/page/1'}>Page2</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={'/page/2'}>Page3</NavLink></S.NavWrapper>
+                </div>глубокая копия
+                <div className={styles.content}> {/*стили для страниц*/}
+                    <Outlet/> {/* Outlet это наш children: [] из файла router.tsx*/} {/* ВСЕГДА МЕНЯЕТСЯ ВНУТРЕННОСТЬ СТРАНИЦЫ КОНТЕНТ СОГЛАСНО ВЫБРАННОЙ СТРАНИЦЫ ПОЛЬЗОВАТЕЛЕМ*/}
+                </div>
+            </div>
         </div>
     );
 }
 
 
 export default App;
+
+
+// в App в родителе(бате) у нас находится то, что всегда не будет изменяться во время route(инга), статичная часть нашей страницы HEADER, FOOTER, NAVLINK
+// Outlet это та часть, которая всегда МЕНЯЕТСЯ и зависит от route(инга)
+/*
+function App() {
+    return (
+<div>
+    <div>
+        HEADER {/!*остается всегда при обновлении страницы сайта*!/}
+    </div>
+        <Outlet/> {/!* Outlet это наш children: [] из файла router.tsx*!/} {/!* ВСЕГДА МЕНЯЕТСЯ ВНУТРЕННОСТЬ СТРАНИЦЫ КОНТЕНТ СОГЛАСНО ВЫБРАННОЙ СТРАНИЦЫ ПОЛЬЗОВАТЕЛЕМ*!/}
+        {/!*<Site/>*!/}
+    <div>
+        FOOTER {/!*остается всегда при обновлении страницы сайта*!/}
+    </div>
+</div>
+    );
+}*/
+
---
+++ src/routes/router.tsx
@@ -0,0 +1,21 @@
+import App from "../App";
+import {Error404} from "../components/pages/Error404";
+import {dataState} from "../data/dataState";
+import {Page} from "../components/pages/Page";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <Error404/>,
+        children: [
+            {
+                path: "/page/:id",
+                element: (
+                    <Page pages={dataState.pages}/>
+                ),
+            },
+        ],
+    },
+]);
